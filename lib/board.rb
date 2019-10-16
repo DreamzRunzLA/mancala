a3befaad76cf12e28fe93da1f566651de1743989
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-10-16 14:48:08 -0600

    Finished first few tests

--- lib/board.rb
+++ lib/board.rb
@@ -2,16 +2,36 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @cups = Array.new(14) {Array.new()}
+    place_stones
   end
 
   def place_stones
-    # helper method to #initialize every non-store cup with four stones each
+    @cups = @cups.each_with_index do |cup, i|
+      if i == 6 || i == 13
+        next
+      else
+        cup << :stone
+        cup << :stone
+        cup << :stone
+        cup << :stone
+      end
+    end
   end
 
   def valid_move?(start_pos)
+    if start_pos < 0 || start_pos > 13
+      raise "Invalid starting cup"
+    elsif @cups[start_pos]
+      raise "Starting cup is empty"
+    else
+      return
+    end
   end
 
   def make_move(start_pos, current_player_name)
+    number_to_move = @cups[start_pos].length
+    
   end
 
   def next_turn(ending_cup_idx)
